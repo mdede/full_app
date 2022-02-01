7d45239b7f2a53f2a6afd7897cf6e8bb900d1481
tmp <tmp@tmp.tmp>
2022-02-01 19:21:36 +0100

    add logging

--- imports/startup/both/index.js
+++ imports/startup/both/index.js
@@ -1,2 +1,6 @@
 // Import modules used by both client and server through a single index entry point
 // e.g. useraccounts configuration file.
+
+import { Log } from 'meteor/logging';
+
+Log.info("Starting up the app.");
