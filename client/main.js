8a40836c86c9873c8d6ece88d9db272412c9af91
tmp <tmp@tmp.tmp>
2022-02-01 20:31:24 +0100

    add pure.css

--- client/main.js
+++ client/main.js
@@ -2,3 +2,6 @@
 
 import '/imports/startup/client';
 import '/imports/startup/both';
+
+import "/node_modules/purecss/build/pure-min.css";
+import "/node_modules/purecss/build/grids-responsive-min.css";
