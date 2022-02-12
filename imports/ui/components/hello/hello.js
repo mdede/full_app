cb2dff9dfc9b277683a48ac93da578dd25512a03
tmp <tmp@tmp.tmp>
2022-02-12 20:10:43 +0100

    user logon logoff

--- imports/ui/components/hello/hello.js
+++ imports/ui/components/hello/hello.js
@@ -1,6 +1,9 @@
 import './hello.html';
 import { Meteor } from 'meteor/meteor';
 
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!getUser();
+
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -10,14 +13,23 @@ Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
-  gitCommitHash() {
-    return Meteor.gitCommitHash;
+//  gitCommitHash() {
+//    return Meteor.gitCommitHash;
+//  },
+  isUserLogged() {
+    return isUserLogged();
   },
+    getUser() {
+      return getUser();
+    },
 });
 
 Template.hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+  },
+  'click #logout'() {
+    Meteor.logout();
   },
 });
