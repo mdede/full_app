ede0fd34a9b463490b34416d2eef11f0dcab6365
tmp <tmp@tmp.tmp>
2022-02-12 20:10:43 +0100

    user logon logoff

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,6 +3,7 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 // Import needed templates
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/login/login.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
@@ -12,6 +13,14 @@ FlowRouter.route('/', {
     this.render('App_body', 'App_home');
   },
 });
+
+FlowRouter.route('/login', {
+  name: 'App.login',
+  action() {
+    this.render('App_body', 'App_login');
+  },
+});
+
 
 FlowRouter.route('*', {
   action() {
---
+++ imports/ui/pages/login/login.js
@@ -0,0 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+import './login.html';
+
+Template.App_login.events({
+  'submit .login-form'(e) {
+    e.preventDefault();
+
+    const target = e.target;
+
+    const username = target.username.value;
+    const password = target.password.value;
+
+    Meteor.loginWithPassword(username, password);
+    FlowRouter.go('App.home');
+  }
+});
