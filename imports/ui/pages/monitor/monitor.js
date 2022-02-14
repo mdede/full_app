e921f0fa8917c6b641022aea3f1be5e5b73b363d
tmp <tmp@tmp.tmp>
2022-02-14 16:08:33 +0100

    add clock

--- imports/ui/pages/monitor/monitor.js
+++ imports/ui/pages/monitor/monitor.js
@@ -1,1 +1,23 @@
+import { Session } from 'meteor/session'
 import './monitor.html';
+
+Template.Monitor.onCreated(function () {
+    // update time every 1 second
+    setInterval(function () {
+        const d = new Date;
+        Session.set("time", String(d.getHours())+":"+String(d.getMinutes()).padStart(2, '0'));
+    }, 1000);
+});
+
+Template.Monitor.helpers({
+  time() {
+    return Session.get("time");
+    },
+  mon() {
+    return {
+        A: 54,
+        P: 46,
+        AP: (46/54*100).toFixed(1)
+        }
+  },
+});
