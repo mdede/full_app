9b257381f99fb7a2597dcddf9357e9e7138a56c9
tmp <tmp@tmp.tmp>
2022-02-15 10:08:21 +0100

    add favicon.ico

--- client/main.js
+++ client/main.js
@@ -3,5 +3,5 @@
 import '/imports/startup/client';
 import '/imports/startup/both';
 
-import "/node_modules/purecss/build/pure-nr-min.css";
+import "/node_modules/purecss/build/pure-min.css";
 import "/node_modules/purecss/build/grids-responsive-min.css";
