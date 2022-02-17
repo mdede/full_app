dbb595b11437f67ed1a105e80ac88de44f81daf4
tmp <tmp@tmp.tmp>
2022-02-17 20:14:31 +0100

    add plus icon to show/hide new form

--- imports/api/links/methods.js
+++ imports/api/links/methods.js
@@ -5,17 +5,50 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import SimpleSchema from 'simpl-schema';
 import { Links } from './links.js';
 
-export const linksInsert = new ValidatedMethod({
-  name: 'links.insert',
-  validate: new SimpleSchema({
+const linksSchema = new SimpleSchema({
+    _id: { type: String, regEx: SimpleSchema.RegEx.Id },
     title: { type: String },
     url: { type: String }
-  }).validator(),
+    });
+
+export const linksInsert = new ValidatedMethod({
+  name: 'links.insert',
+  validate: linksSchema.omit("_id").validator({ clean: true, filter: false }),
   run({ title, url }) {
-    return Links.insert({
-      url,
-      title,
+    if (!this.userId) {
+      // Throw errors with a specific error code
+      throw new Meteor.Error('links.insert.notLoggedIn', 'Must be logged in to add links.');
+    }
+    return Links.insert({ url, title,
       createdAt: new Date(),
+      lastUpdatedAt: new Date(),
     });
   }
 });
+
+export const linksUpdate = new ValidatedMethod({
+  name: 'links.update',
+  validate: linksSchema.validator({ clean: true }),
+  run(doc) {
+    if (!this.userId) {
+      throw new Meteor.Error('links.update.notLoggedIn', 'Must be logged in to update links.');
+    }
+    return Links.update(doc._id, {$set: {
+        url: doc.url,
+        title: doc.title,
+        lastUpdatedAt: new Date(),
+    }});
+  }
+});
+
+export const linksDelete = new ValidatedMethod({
+  name: 'links.delete',
+  validate: linksSchema.pick("_id").validator({ clean: true }),
+  run(doc) {
+    if (!this.userId) {
+      throw new Meteor.Error('links.update.notLoggedIn', 'Must be logged in to delete links.');
+    }
+    return Links.remove(doc._id);
+  }
+});
+
--- imports/ui/components/info/info.js
+++ imports/ui/components/info/info.js
@@ -1,6 +1,6 @@
 import { Links } from '/imports/api/links/links.js';
 import { Meteor } from 'meteor/meteor';
-import { linksInsert } from '/imports/api/links/methods.js';
+import { linksInsert, linksUpdate, linksDelete } from '/imports/api/links/methods.js';
 import './info.html';
 
 Template.info.onCreated(function () {
@@ -9,16 +9,21 @@ Template.info.onCreated(function () {
 //    this.subscribe('todos.inList', this.getListId());
       this.subscribe('links.all');
   });
+//  _id of edited link
+  this.edit_id = new ReactiveVar('');
 });
 
 Template.info.helpers({
   links() {
-    return Links.find({});
+    return Links.find({}, { sort: { lastUpdatedAt: -1 } });
   },
+  isEdited(_id) {
+    return _id === Template.instance().edit_id.get() ? true : false;
+  }
 });
 
 Template.info.events({
-  'submit .info-link-add'(event) {
+  'submit .info-link-add' (event, instance) {
     event.preventDefault();
 
     const target = event.target;
@@ -32,9 +37,46 @@ Template.info.events({
       if (err) {
         alert(err);
       } else {
-        title.value='';
-        url.value='';
+          title.value='';
+          url.value='';
+          instance.edit_id.set('');
       }
     });
+  },
+    'submit .info-link-update' (event, instance) {
+      event.preventDefault();
+      const target = event.target;
+      linksUpdate.call({
+        _id: Template.instance().edit_id.get(),
+        title: target.title.value,
+        url: target.url.value
+      }, (err, res) => {
+        if (err) {
+          alert(err);
+        } else {
+          instance.edit_id.set('');
+        }
+      });
+    },
+
+    'click .remove-link' (event, instance) {
+      linksDelete.call({
+        _id: event.target.getAttribute('data')
+      }, (err, res) => {
+        if (err) {
+          alert(err);
+        } else {
+          instance.edit_id.set('');
+        }
+      });
+
+    },
+  'click .edit_cancel' (event, instance) {
+    event.preventDefault();
+    instance.edit_id.set('');
+  },
+  'click .edit-link, click .add-link' (event, instance) {
+    const _id = event.target.getAttribute('data');
+    instance.edit_id.set(_id);
   },
 });
