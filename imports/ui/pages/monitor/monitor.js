f61f0d0b8a20aed4e686f8fef1e0a7df52b14db2
tmp <tmp@tmp.tmp>
2022-02-19 18:45:02 +0100

    sekundy bar

--- imports/ui/pages/monitor/monitor.js
+++ imports/ui/pages/monitor/monitor.js
@@ -1,18 +1,34 @@
-import { Session } from 'meteor/session'
+import { Meteor } from 'meteor/meteor';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import './monitor.html';
 
+var dateVar = new ReactiveDict();
+
 Template.Monitor.onCreated(function () {
+    dateVar.setDefault({
+        hh_mm: '00:00',
+        ss: 00,
+    });
     // update time every 1 second
-    setInterval(function () {
+    this.intervalJob = Meteor.setInterval(function () {
         const d = new Date;
-        Session.set("time", String(d.getHours())+":"+String(d.getMinutes()).padStart(2, '0'));
+        dateVar.set('hh_mm', String(d.getHours())+":"+String(d.getMinutes()).padStart(2, '0'));
+        dateVar.set('ss', (1+d.getSeconds())*100/60);
     }, 1000);
+});
+
+Template.Monitor.onDestroyed(function () {
+    Meteor.clearInterval(this.intervalJob);
+    dateVar.destroy();
 });
 
 Template.Monitor.helpers({
   time() {
-    return Session.get("time");
+    return dateVar.get('hh_mm');
     },
+  bar_percent() {
+    return dateVar.get('ss');
+  },
   mon() {
     return {
         A: 54,
