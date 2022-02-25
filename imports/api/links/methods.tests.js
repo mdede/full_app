e066c7d1eb1a140d0e48c637ddc946c3fa1085cf
tmp <tmp@tmp.tmp>
2022-02-25 09:47:51 +0100

    fix tests

--- imports/api/links/methods.tests.js
+++ imports/api/links/methods.tests.js
@@ -13,35 +13,35 @@ if (Meteor.isServer) {
       Links.remove({});
     });
 
-    it('can not add incorrect link Title', function () {
-        assert.throws(() => {
-            linksInsert.validate({title: 1, url: 'https://www.meteor.com'});
-        }, undefined, /must be of type String/);
-    });
-    it('can not add incorrect link Url', function () {
-        assert.throws(() => {
-            linksInsert.validate({title: "String OK", url: -100});
-        }, undefined, /must be of type String/);
-    });
-    it('can add a new link', function () {
-        assert.doesNotThrow (() => {
-            linksInsert.validate({title: 'meteor.com', url: 'https://www.meteor.com'});
-        });
-    });
-    it('accepts and filter our extra attr', function () {
-        assert.throws (() => {
-            linksInsert.validate({title: 'meteor.com', url: 'https://www.meteor.com', extra: 1});
-        }, undefined, /is not allowed by the schema/);
-    });
-    it('not logged on, throws error', function () {
+    it('linksInsert - not logged on, throws error', function () {
         assert.throws (() => {
             linksInsert.call({title: 'meteor.com', url: 'https://www.meteor.com'});
         }, undefined, /Must be logged in to/);
     });
-    it('logged on with userId', function () {
+    it('linksInsert - logged on with userId', function () {
         assert.doesNotThrow (() => {
             linksInsert._execute({ userId: "j8H12k9l98UjL" }, {title: 'meteor.com', url: 'https://www.meteor.com'});
         });
+    });
+    it('linksInsert - can not add incorrect link Title', function () {
+        assert.throws(() => {
+            linksInsert.validate({title: 1, url: 'https://www.meteor.com'});
+        }, undefined, /must be of type String/);
+    });
+    it('linksInsert - can not add incorrect link Url', function () {
+        assert.throws(() => {
+            linksInsert.validate({title: "String OK", url: -100});
+        }, undefined, /must be of type String/);
+    });
+    it('linksInsert - can add a new link', function () {
+        assert.doesNotThrow (() => {
+            linksInsert.validate({title: 'meteor.com', url: 'https://www.meteor.com'});
+        });
+    });
+    it('linksInsert - accepts and filter our extra attr', function () {
+        assert.throws (() => {
+            linksInsert.validate({title: 'meteor.com', url: 'https://www.meteor.com', extra: 1});
+        }, undefined, /is not allowed by the schema/);
     });
 
   });
