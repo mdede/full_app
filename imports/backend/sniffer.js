9e581b5d6916e6dcb721f8bbe4bef03dbf896b9a
tmp <tmp@tmp.tmp>
2022-03-03 20:25:41 +0100

    cleanup sniffer.js

--- imports/backend/sniffer.js
+++ imports/backend/sniffer.js
@@ -9,31 +9,30 @@ import moment from 'moment';
 Meteor.startup(() => {
     if(Meteor.settings.snifferSchedule) {
         initPool();
+        SyncedCron.add({
+            name: 'cleanup',
+            schedule: parser => parser.text('every 1 hour'),
+            job: () => cleanup(),
+        });
+        SyncedCron.start();
     }
 });
 
-async function initPool() {
+function initPool() {
   const oraConnectionJSON = JSON.parse(Assets.getText('OracleConnection.json'));
   Log.info("Init oracledb at "+oraConnectionJSON.libDir+", connect to: "+oraConnectionJSON.user+"@"+oraConnectionJSON.connectString);
   try {
    oracledb.initOracleClient({libDir: oraConnectionJSON.libDir});
-   await oracledb.createPool(oraConnectionJSON);
+   oracledb.createPool(oraConnectionJSON);
    Log.debug('got pool');
   } catch (err) {
            Log.error('initPool error: '+err.errorNum+": "+err.message);;
   } finally {
-//      Meteor.setInterval(()=>runExample (), Meteor.settings.snifferTimeout);
           SyncedCron.add({
             name: 'query oracle',
             schedule: parser => parser.text(Meteor.settings.snifferSchedule, true),
             job: () => runExample(),
           });
-          SyncedCron.add({
-            name: 'cleanup',
-            schedule: parser => parser.text('every 1 hour'),
-            job: () => cleanup(),
-          });
-          SyncedCron.start();
   }
 }
 
@@ -42,29 +41,19 @@ async function runExample() {
   try {
     connection = await oracledb.getConnection(); //from the pool
     binds = {};
-    // For a complete list of options see the documentation.
     options = {
       outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
-      // extendedMetaData: true,               // get extra metadata
-      // prefetchRows:     100,                // internal buffer allocation size for tuning
-      // fetchArraySize:   100                 // internal buffer allocation size for tuning
     };
-    //
-    // Show the date.  The value of ORA_SDTZ affects the output
-    //
     sql = `SELECT TO_CHAR(SYSTIMESTAMP, 'YYYY-MM-DD HH24:MI:SS.FF') AS CD FROM DUAL`;
     result = await connection.execute(sql, binds, options);
 //    Log.debug("Metadata: ");
 //    Log.debug(result.metaData, { depth: null });
-    Log.debug("Query results: ");
-    Log.debug(result.rows, { depth: null });
-
+//    Log.debug("Query results: ");
+//    Log.debug(result.rows, { depth: null });
     Links.update({url: "http://seznam.cz"}, {$set: {title: result.rows[0]['CD']}});
-
   } catch (err) {
     Log.error('Sniffer oracle: '+err.errorNum+": "+err.message);
-  }
-  finally {
+  } finally {
     if (connection) {
       try {
         await connection.release();
